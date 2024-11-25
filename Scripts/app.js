2d290639398cb893fd97b10a194023d271edca65
NushK650 <Nush8653@gmail.com>
2024-11-25 13:05:16 -0800

    second commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,7 +1,20 @@
-// store users box choice
-let boxPick = document.getElementById(boxNumber)
-// store users color choice
-let colorChoice = document.getElementById(changeColor)
-// 
-//  
+
+let boxNumber = document.getElementById("boxNumber")
+
+let changeColor = document.getElementById("changeColor")
+let box = document.getElementById('box${boxNumber}')
+let box3 = document.getElementById("box3")
+let box4 = document.getElementById("box4")
+let box5 = document.getElementById("box5")
+let box6 = document.getElementById("box6")
+let box7 = document.getElementById("box7")
+let box8 = document.getElementById("box8")
+
+
+ 
+
+let changeColorbtn = document.getElementById("changeColorBtn")
+let resetBtnbtn = document.getElementById("resetBtn")
+
+
 
